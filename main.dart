e959786e8dc01d9a29f39e4ca9c7df9eaf0d4a1f
広田　響 <35335011+teimusheru@users.noreply.github.com>
2022-09-08 11:35:33 +0900

    日毎　leftから　  曜日日本語

---
+++ main.dart
@@ -0,0 +1,111 @@
+
+import 'dart:math';
+
+import 'package:calendar_agenda/calendar_agenda.dart';
+import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_event_calendar/flutter_event_calendar.dart';
+
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ExamplePage(),
+    );
+  }
+}
+
+class ExamplePage extends StatefulWidget {
+  const ExamplePage({Key? key}) : super(key: key);
+
+  @override
+  _ExamplePageState createState() => _ExamplePageState();
+}
+
+class _ExamplePageState extends State<ExamplePage> {
+  CalendarAgendaController _calendarAgendaControllerAppBar =
+  CalendarAgendaController();
+  CalendarAgendaController _calendarAgendaControllerNotAppBar =
+  CalendarAgendaController();
+
+  late DateTime _selectedDateAppBBar;
+  late DateTime _selectedDateNotAppBBar;
+
+  Random random = new Random();
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedDateAppBBar = DateTime.now();
+    _selectedDateNotAppBBar = DateTime.now();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CalendarAgenda(
+        controller: _calendarAgendaControllerAppBar,
+        appbar: true,
+        selectedDayPosition: SelectedDayPosition.left,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back_ios_new,
+            color: Colors.white,
+          ),
+          onPressed: () {_calendarAgendaControllerAppBar.goToDay(DateTime.now());},
+        ),
+        weekDay: WeekDay.long,
+        fullCalendarScroll: FullCalendarScroll.horizontal,
+        fullCalendarDay: WeekDay.long,
+        selectedDateColor: Colors.green.shade900,
+        locale: 'ja_JP',
+        initialDate: DateTime.now(),
+        calendarEventColor: Colors.green,
+        firstDate: DateTime.now().subtract(Duration(days: 0)),
+        lastDate: DateTime.now().add(Duration(days: 180)),
+        events: List.generate(
+            100,
+                (index) => DateTime.now()
+                .subtract(Duration(days: index * random.nextInt(5)))),
+        onDateSelected: (date) {
+          setState(() {
+            _selectedDateAppBBar = date;
+          });
+        },
+        calendarLogo: Image.network(
+          'https://www.kindpng.com/picc/m/355-3557482_flutter-logo-png-transparent-png.png',
+          scale: 5.0,
+        ),
+        selectedDayLogo: NetworkImage(
+          'https://www.kindpng.com/picc/m/355-3557482_flutter-logo-png-transparent-png.png',
+          scale: 15.0,
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+
+            SizedBox(
+              height: 20.0,
+            ),
+            Text('aaaaaaaaaaaaa'),
+
+
+          ],
+        ),
+      ),
+    );
+  }
+}
